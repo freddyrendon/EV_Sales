8979e655d889fe4c0aca6fbe427361baabb3dd1d
Freddy Rendon <frendonjr03@gmail.com>
2023-02-03 17:33:56 -0500

    added text

--- src/scripts/usMap.js
+++ src/scripts/usMap.js
@@ -17,40 +17,93 @@ class Usmap {
             .scale([1000]); // scale things down so see entire US
 
         const path = d3.geoPath().projection(projection);
-        // debugger
         d3.json("https://gist.githubusercontent.com/Bradleykingz/3aa5206b6819a3c38b5d73cb814ed470/raw/a476b9098ba0244718b496697c5b350460d32f99/us-states.json", function (error, uState) {
             if (error) throw error;
 
             svg.selectAll('path')
                 .data(uState.features)
-                // .data(uState.properties.name)
                 .enter()
                 .append('path')
-                // .append('text')
                 .attr("d", path)
                 .attr('class','state')
-                // .attr('state', uState.features[0].properties.name)
-                // .text(function() {
-                //     return d
-                // })
-                // debugger
 
-
-                // 
-                // debugger
                 .text(svg.selectAll('.state')._groups[0]
                 .forEach(element => {
-                    element.addEventListener('mouseover', function (){
-                        // return element.__data__.properties.name
-                        return (element.__data__.properties.name)
-                        // const barGraph = new Bargraph(element.__data__.properties.name)
+                    element.addEventListener('mouseover', function (e){
+                        var rect = e.target.getBoundingClientRect();
+                        var x = e.clientX - rect.left; //x position within the element.
+                        var y = e.clientY - rect.top;  //y position within the element.
+
+                        d3.select("#tooltip")
+                            .style("left", x + "px")
+                            .style("top", y + "px")
+                            .style("display", "block")
+                            .html("State Name: " + element.__data__.properties.name)
+                            // .html("Auto Manufactour: Tesla");
+
+
+                        console.log(element.__data__.properties.name)
                     })
                     
                 }))
-                debugger
 
                 
             });
+        // d3.json("https://gist.githubusercontent.com/Bradleykingz/3aa5206b6819a3c38b5d73cb814ed470/raw/a476b9098ba0244718b496697c5b350460d32f99/us-states.json", function (error, uState) {
+        //     if (error) throw error;
+
+        //     svg.selectAll('path')
+        //         .data(uState.features)
+        //         .enter()
+        //         .append('path')
+        //         .attr("d", path)
+        //         .attr('class', 'state');
+
+        //     svg.selectAll("text")
+        //         .data(uState.features)
+        //         .enter()
+        //         .append("text")
+        //         .attr("x", function (d) { return path.centroid(d)[0]; })
+        //         .attr("y", function (d) { return path.centroid(d)[1]; })
+        //         .text(function (d) { return d.properties.name; })
+        //         .style("text-anchor", "middle")
+        //         .style("font-size", "10px")
+        //         .style("fill", "black");
+        // });
+
+//     d3.json("https://gist.githubusercontent.com/Bradleykingz/3aa5206b6819a3c38b5d73cb814ed470/raw/a476b9098ba0244718b496697c5b350460d32f99/us-states.json", function (error, uState) {
+//     if (error) throw error;
+
+//   svg.selectAll('path')
+//     .data(uState.features)
+//     .enter()
+//     .append('path')
+//     .attr("d", path)
+//     .attr('class','state')
+//     .on("mouseover", function(d) {
+//       d3.select("#state-name-" + d.id)
+//         .style("display", "block");
+//     })
+//     .on("mouseout", function(d) {
+//       d3.select("#state-name-" + d.id)
+//         .style("display", "none");
+//     });
+
+//   svg.selectAll("text")
+//     .data(uState.features)
+//     .enter()
+//     .append("text")
+//     .attr("x", function(d) { return path.centroid(d)[0]; })
+//     .attr("y", function(d) { return path.centroid(d)[1]; })
+//     .text(function(d) { return d.properties.name ; })
+//     .style("text-anchor", "middle")
+//     .style("font-size", "10px")
+//     .style("fill", "black")
+//     .style("display", "none")
+//     .attr("id", function(d) { return "state-name-" + d.id; });
+// });
+
+
 
             
     }
